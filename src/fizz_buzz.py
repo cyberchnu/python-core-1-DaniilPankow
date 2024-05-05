a0ceeae8fba7e1f9bb527fc2ffb5b7b6f8916445
DaniilPankow <pankov.daniil@chnu.edu.ua>
2024-05-05 19:58:27 +0300

    Complete 3

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,3 +1,12 @@
 def fizz_buzz(param):
   #Type your code here
-  return
+    if param//3 and param//5:
+      return "FizzBuzz"
+    elif param//3:
+      return "Fizz"
+    elif param//5:
+      return "Buzz"
+    else:
+      return str(param)
+print(fizz_buzz(2))
+
